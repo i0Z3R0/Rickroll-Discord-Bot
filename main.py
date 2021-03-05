ecd7948e0032ca9f3068379684906af142ba15b3
EA | 0Z3R0 <49484081+i0Z3R0@users.noreply.github.com>
2021-03-05 18:28:11 +0800

    some updates
    added comments to code for better clarity
    added help commands
    fixed some importing stuffs

--- main.py
+++ main.py
@@ -9,10 +9,12 @@ client.remove_command('help')
 
 @client.event
 async def on_ready():
-    print('Online')
+    print('Bot is Online')
 
+# Creates the list that will be used to store all the channels and VCs created
 createdChannels = []
 
+# Command to rick roll
 @client.command(pass_context=True)
 async def rickroll(ctx):
     print(f'Rickrolling {ctx.guild.name}')
@@ -20,6 +22,7 @@ async def rickroll(ctx):
     createdChannels.append(rickrollCat1)
     print("Category Made")
     rickRoll = ["Never gonna give you up", "Never gonna let you down", "Never gonna run around", "and desert you", "Never gonna", "make you cry", "Never gonna say goodbye", "Never gonna tell a lie", "and hurt you"]
+    # Iterates over every item in the list rickRoll and creates a channel with the name as the current iteration
     for i in rickRoll:
         currentChannel = await ctx.guild.create_text_channel(i, category=rickrollCat1)
         await currentChannel.send("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
@@ -28,10 +31,13 @@ async def rickroll(ctx):
     print(f'Completed Rick Rolling Text Channels')
     rickrollCat2 = await ctx.guild.create_category("Rick Roll")
     createdChannels.append(rickrollCat2)
+    # Does the same as before but with VCs
     for i in rickRoll:
         currentVC = await ctx.guild.create_voice_channel(i, category=rickrollCat2)
         createdChannels.append(currentVC)
+    print(f'Completed Rick Rolling VCs')
 
+# Deletes all created channels
 @client.command(pass_context=True)
 async def unroll(ctx):
     for j in createdChannels:
@@ -39,15 +45,24 @@ async def unroll(ctx):
             await j.delete()
         except:
             return
-    print(f'Deleted {len(createdChannels)} Rickroll Channels in {ctx.guild.name} ')
+    print(f'Deleted {len(createdChannels)} Rickroll Channels')
 
+# Shows all the commands
 @client.command(pass_context=True)
 async def help(ctx):
-    ctx.send("Commands: !rickroll Rick rolls the servers \n !unroll Deletes all rick roll channels")
+    channel = ctx.channel
+    embedVar = discord.Embed(title="Commands", description="", color=0x00ff00)
+    embedVar.add_field(name="!rickroll", value="Rick rolls the server", inline=False)
+    embedVar.add_field(name="!unroll", value="Deletes all rick roll channels", inline=False)
+    embedVar.add_field(name="!help", value="Shows this message", inline=False)
+    embedVar.add_field(name="!ping", value="Displays latency of bot in seconds", inline=False)
+    await channel.send(embed=embedVar)
     print("Help has been sent")
 
+# Shows latency of the bot
 @client.command(pass_context=True)
 async def ping(ctx):
-    await ctx.send(f'Pong! {bot.latency}!')
+    channel = ctx.channel
+    await channel.send(f'Pong! {client.latency}!')
 
 client.run(token, bot = True)
